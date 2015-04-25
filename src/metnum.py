e9da8a1493784d2628c298c863b906d8f732686d
Julian Bayardo <julian.bayardo@mercadolibre.com>
2015-04-25 01:16:11 -0300

    La posta para empezar como se debe

--- src/metnum.py
+++ src/metnum.py
@@ -11,7 +11,7 @@ def build():
 
 def compile():
   for source in sources:
-    run(compiler, '-O2', '-c', '--std=c++11', '-ggdb', source+'.cpp', '-o', source+'.o')
+    run(compiler, '-O2', '-Ieigen', '-c', '--std=c++11', '-ggdb', source+'.cpp', '-o', source+'.o')
 
 def link():
   objects = [s+'.o' for s in sources]
